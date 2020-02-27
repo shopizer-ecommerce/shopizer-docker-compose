e064b1025db1b67eba44545da19988c3ea4328c0
Shopizer <csamson777@yahoo.com>
2020-02-27 14:33:08 -0500

    Update lightsail-install.sh

--- lightsail-install.sh
+++ lightsail-install.sh
@@ -14,11 +14,11 @@ chmod +x /usr/local/bin/docker-compose
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
-sudo curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/shopizer-ecommerce/shopizer-lightsail/master/docker-compose.yml?token=ACt88_ywRxm6UnvRw6mLOKOKgmfjG3h3ks5clpRIwA%3D%3D
+sudo curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/shopizer-ecommerce/shopizer-docker-compose/master/docker-compose.yml?token=ACt88_ywRxm6UnvRw6mLOKOKgmfjG3h3ks5clpRIwA%3D%3D
 
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
-sudo curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/shopizer-ecommerce/shopizer-aws-lightsail/master/docker-compose-app.service
+sudo curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/shopizer-ecommerce/shopizer-docker-compose/master/docker-compose-app.service
 
 sudo chmod 755 /etc/systemd/system/docker-compose-app.service
 sudo systemctl enable docker-compose-app
